8f71dbaa4097509847670b69562360001112347b
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-09-20 20:32:12 +1000

    Adding database health check

--- src/main/java/com/ruchij/services/health/HealthServiceImpl.java
+++ src/main/java/com/ruchij/services/health/HealthServiceImpl.java
@@ -2,24 +2,30 @@ package com.ruchij.services.health;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ruchij.services.health.models.HealthCheck;
+import com.ruchij.services.health.models.HealthStatus;
 import com.ruchij.services.health.models.ServiceInformation;
 import com.ruchij.services.system.SystemService;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class HealthServiceImpl implements HealthService {
     private static final String INFORMATION_FILE = "information.json";
 
+    private final JdbcTemplate jdbcTemplate;
     private final SystemService systemService;
     private final ObjectMapper objectMapper;
 
-    public HealthServiceImpl(SystemService systemService, ObjectMapper objectMapper) {
+    public HealthServiceImpl(JdbcTemplate jdbcTemplate, SystemService systemService, ObjectMapper objectMapper) {
+        this.jdbcTemplate = jdbcTemplate;
         this.systemService = systemService;
         this.objectMapper = objectMapper;
     }
@@ -47,7 +53,23 @@ public class HealthServiceImpl implements HealthService {
 
     @Override
     public HealthCheck healthCheck() {
-        return null;
+        CompletableFuture<HealthStatus> databaseHealthCheck = CompletableFuture.supplyAsync(this::database);
+        HealthStatus database = waitUntilTimeout(databaseHealthCheck);
+
+        return new HealthCheck(database);
+    }
+
+    private HealthStatus database() {
+        Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+        return result == 1 ? HealthStatus.HEALTHY : HealthStatus.UNHEALTHY;
+    }
+
+    private HealthStatus waitUntilTimeout(CompletableFuture<HealthStatus> healthStatus) {
+        try {
+            return healthStatus.get(10, TimeUnit.SECONDS);
+        } catch (Exception exception) {
+            return HealthStatus.UNHEALTHY;
+        }
     }
 
     @Setter
